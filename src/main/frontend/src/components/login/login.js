8f3605066b30362858753706b621c12d2d57e2b2
ssl82 <ssl82@naver.com>
2022-10-31 01:47:49 +0900

    예제 생성

--- src/main/frontend/src/components/login/login.js
+++ src/main/frontend/src/components/login/login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 import './login.css'
 import heart from '../../img/el_heart.svg'
 import bar from '../../img/bar.svg'
@@ -7,6 +8,21 @@ import right from '../../img/right.svg'
 import login from '../../img/login.svg'
 
 function Login(){
+    let data = {
+        userid : 'test'
+        ,userPassWord : 'test00'
+        ,idx : 0
+    }
+
+
+    const handleInputId = (e) => {
+        data.userid = e.target.value;
+    }
+
+    function handleInputPw(e) {
+        data.userPassWord = e.target.value;
+    }
+
     return(
         <>
         <div className="back_img">
@@ -15,9 +31,9 @@ function Login(){
                     <div className='logintext'>
                         <div className='title'>LOGIN</div>
                         <div className='text'>이메일</div>
-                        <input type={'email'} className='emailbox'></input>
+                        <input type={'email'} className='emailbox' defaultValue={data.userid} onChange={handleInputId}></input>
                         <div className='text'>비밀번호</div>
-                        <input type={'password'} className='passwordbox'></input>
+                        <input type={'password'} className='passwordbox' defaultValue ={data.userPassWord} onChange={handleInputPw}></input>
                     </div>
                 </div>
             </div>
@@ -36,7 +52,26 @@ function Login(){
                     <div className='row'>
                         <img src={left}></img>
 
-                        <img className='btn' src={login} onClick={()=>{alert('비밀번호가 정확하지 않습니다 다시 시도해주세요')}}></img>
+                        <img className='btn' src={login} onClick={()=>{
+                            axios.post( '/memberCreate',
+                                {
+                                    MEM_ID: data.userid
+                                    , MEM_PW : data.userPassWord
+                                    , MEM_NIKNE : '누구니너'
+                                    , MEM_CALL_NUM : '01012345678'
+                                },
+                                {
+                                    headers:{
+                                        // 'Content-type': 'application/json',
+                                        // 'Accept': 'application/json',
+                                        contentType: 'application/json'
+                                    }
+                                }
+                            )
+                            .then((response) => { console.log(response.data); })
+                            .catch((response) => { console.log('Error!') });
+                            alert('비밀번호가 정확하지 않습니다 다시 시도해주세요')
+                        }}></img>
                         <img src={right}></img>
                     </div>
                 </div>
