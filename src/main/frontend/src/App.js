fc5612cfd0ea4632964234cd1a3350245ce6cc5f
ssl82 <ssl82@naver.com>
2022-11-01 00:53:22 +0900

    수정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,16 @@
 import Login from './components/login/login.js';
 import Signin from './components/Signin/signin.js';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Signin></Signin>
+        <Routes>
+        <Route exact path="/Login" element={<Login/>}/>
+        <Route exact path="/Signin" element={<Signin/>}/>
+        </Routes>
     </div>
+
   );
 }
 
