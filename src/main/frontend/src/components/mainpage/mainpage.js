75e8986180c2f2b840e925c34043b3547bf48394
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2022-11-04 00:19:42 +0900

    1103 fix:main

--- src/main/frontend/src/components/mainpage/mainpage.js
+++ src/main/frontend/src/components/mainpage/mainpage.js
@@ -4,12 +4,36 @@ import './mainpage.css'
 import logo from '../../img/logo.svg'
 import search from '../../img/search.svg'
 import musicimg from '../../img/musicimg.jpg'
+import chartimg from'../../img/chart.svg'
+import playbtn from '../../img/playbtn.svg'
+import infobtn from '../../img/infobtn.svg'
+import rankingup from '../../img/rankingup.svg'
 function Main(){
     return(
         <>
         <div className="back">
             <Sidebar></Sidebar>
-            <div className="chart">chart</div>
+            <div className="chart">
+                <div className="charttitle">오늘의 차트</div>
+                <div className="chartscreen">
+                    <img src={chartimg}></img>
+                    <div className="charttext">
+                        <div className="chart_rankingbox">
+                            <div className="chart_ranking">차트 1위</div>
+                            <img src={rankingup}></img>
+                        </div>
+                        <div className='chart_musictitle'>After Like</div>
+                        <div className='chart_musicartist'>IVE(아이브)</div>
+                        <div className="chart_btn">
+                            <img src={playbtn} onClick={()=>{alert('재생화면으로 이동합니다')}}></img>
+                            <img src={infobtn} onClick={()=>{alert('상세정보 화면으로 이동합니다')}}></img>
+                        </div>
+                        <div className="chart_link">차트 자세히 보러가기></div>
+
+                    </div>
+                </div>
+                <div className="charttitle">당신이 원하는 음악</div>
+            </div>
         </div>
         </>
     )
--- src/main/frontend/src/components/sidebar/sidebar.js
+++ src/main/frontend/src/components/sidebar/sidebar.js
@@ -3,14 +3,45 @@ import './sidebar.css'
 import logo from '../../img/logo.svg'
 import search from '../../img/search.svg'
 import musicimg from '../../img/musicimg.jpg'
+import profile from '../../img/profile.svg'
+import loginbtn from '../../img/loginbtn.svg'
+import signinbtn from '../../img/signinbtn.svg'
+import { Link } from "react-router-dom";
+
+import side1 from'../../img/side (1).svg'
+import side2 from'../../img/side (2).svg'
+import side3 from'../../img/side (3).svg'
+import side4 from'../../img/side (4).svg'
+import side5 from'../../img/side (5).svg'
+import side6 from'../../img/side (6).svg'
 
 function Sidebar(){
     return(
         <div className="menu">
             <img className="logo" src={logo}></img>
             <div className="searchplace">
-                <img className="search" src={search} onClick={()=>{alert('검색결과가 존재하지 않습니다.')}}></img>
-                <input placeholder="PLURA검색" type={'text'} className="searchbox"></input>
+            <img className="search" src={search} onClick={()=>{alert('검색결과가 존재하지 않습니다.')}}></img>
+                <input placeholder="PLURA 검색" type={'text'} className='searchbox'></input>
+            </div>
+            <div className="side_profile">
+                <img src={profile}></img>
+                <div className="guide">로그인이 필요합니다</div>
+            </div>
+            <div className="login_btns">
+                <Link to="/Login">
+                    <img src={loginbtn}></img>
+                </Link>
+                <Link to="/Signin">
+                    <img src={signinbtn}></img>
+                </Link>
+            </div>
+            <div className="side_menu">
+            <img src={side1}></img>
+            <img src={side2}></img>
+            <img src={side3}></img>
+            <img src={side4}></img>
+            <img src={side5}></img>
+            <img src={side6}></img>
             </div>
         </div>
 
