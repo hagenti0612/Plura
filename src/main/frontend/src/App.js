45ab437c68d52d005a223aef6043be2369587973
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2022-11-21 20:40:24 +0900

    1

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,10 @@
 import Login from './components/login/login.js';
 import Signin from './components/Signin/signin.js';
-import Main from './components/mainpage/mainpage.js'
-import Sidebar from './components/sidebar/sidebar.js'
+import Main from './components/mainpage/mainpage.js';
+import Sidebar from './components/sidebar/sidebar.js';
+import Chart from './components/chart/chart.js';
+import Detail from './components/detail/detail.js';
+import Admin_add from './components/register/register.js';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
@@ -10,7 +13,12 @@ function App() {
       <Routes>
         <Route exact path="/Login" element={<Login/>}/>
         <Route exact path="/Signin" element={<Signin/>}/>
-        <Route path="/" element={<Main/>}/>
+        <Route exact path="/Chart" element={<Chart/>}/>
+        <Route exact path="/Detail" element={<Detail/>}/>
+        <Route exact path="/admin_add" element={<Admin_add/>}/>
+        <Route exact path="/main" element={<Main/>}/>
+
+        
       </Routes>
 
     </div>
